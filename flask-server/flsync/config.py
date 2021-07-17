ca9d660d6cfae9588e6e645c51cfc1184d3bfa25
inovas97 <56592438+inovas97@users.noreply.github.com>
2021-07-17 19:52:14 +0300

    Update config.py

--- flask-server/flsync/config.py
+++ flask-server/flsync/config.py
@@ -1,6 +1,6 @@
 
 class Config:
-    SECRET_KEY = '96c370876ecfd8bcc5180457103089ac'
+    SECRET_KEY = 'yourssecretkey'
     MAIL_SERVER='smtp.gmail.com'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
